88880843e543f265b2e43c15a7f56eb565213e7e
omind.io <48690665+omindio@users.noreply.github.com>
2025-01-14 17:06:11 +0100

    Final commit for css technical test

--- prueba-css/src/index.js
+++ prueba-css/src/index.js
@@ -1,26 +1,40 @@
-
 var arrayTest = [1, 2, 3, 3, 2, 1, 4];
 
 document.getElementById("app").innerHTML = `
-  <h1>Unique Element Challenge</h1>
+  <header>
+    <h1>Unique Element Challenge</h1>
+    <img src="src/assets/logo.svg" alt="Logo" />
+  </header>
 
-  <h2>Target</h2>
-  <div>Find the unique value in the array that is not duplicated. Use the <b>dubbed</b> function to return the unique element.</div>
+  <section>
+    <div class="content-left">
+      <h2>Target</h2>
+      <div>Find the unique value in the array that is not duplicated. Use the <b>dubbed</b> function to return the unique element.</div>
 
-  <h2>Considerations</h2>
-  <ul>
-    <li>Values are duplicated only twice.</li>
-    <li>There is only one non duplicate value.</li>
-    <li>The non duplicate value can be placed anywhere on the array.</li>
-    <li>The result must be an integer.</li>
-  </ul>
+      <h2>Considerations</h2>
+      <ul>
+        <li>Values are <b>duplicated only twice</b>.</li>
+        <li>There is <b>only one non duplicate value</b>.</li>
+        <li>The non duplicate value can be placed <b>anywhere on the array</b>.</li>
+        <li>The result must be an <b>integer</b>.</li>
+      </ul>
+    </div>
   
-  <h2>Test Data</h2>
-  <div>${arrayTest}</div>
-    
-  <h2>Expected result</h2>
-  <div>4</div>
+    <div class="content-right">
+      <div class="block">
+        <h2>Test Data</h2>
+        <div>${arrayTest}</div>
+      </div>
+        
+      <div class="block">
+        <h2>Expected result</h2>
+        <div>4</div>
+      </div>
 
-  <h2>Your Result</h2>
-  <div>4</div>
+      <div class="block">
+        <h2>Your Result</h2>
+        <div>4</div>
+      </div>
+    </div>
+  </section>
 `;
